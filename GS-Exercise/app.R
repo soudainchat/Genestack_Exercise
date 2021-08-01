cd68fbda41469fe7806010d41798fc220de5cc3b
soudainchat <malazarenko@gmail.com>
2021-08-02 00:01:07 +0300

    Search SelectizeInput for ontology term output has been added. But it's not working now

--- GS-Exercise/app.R
+++ GS-Exercise/app.R
@@ -9,6 +9,8 @@ ui <- fluidPage(
                        choices = NULL)
     ),
     mainPanel(
+        selectizeInput("genes", "Find gene symbol:", 
+                       choices = NULL),
         helpText('Information found:'),
         htmlOutput("smth")
     )
@@ -31,14 +33,38 @@ server <- function(input, output, session) {
                           "<p>","<b>", "Gene Synonyms: ","</b>", df[df$gene_symbol == input$inpt, "gene_synonyms"], "</p>",
                           "<p>","<b>", "Gene Ontology Term: ","</b>", df[df$gene_symbol == input$inpt, "go_term_label"], "</p>"))
         }
+        
         else if (input$inpt %in% df$go_term_label){ 
-            unique(paste0("<b>", "Associated gene: ", "</b>", 
-                          "<p>", df[df$go_term_label== input$inpt | substr(df$go_term_label, 1, nchar(input$inpt)) == input$inpt, "gene_symbol"], "</p>", 
-                          "<b>", " Synonyms: ", "</b>", 
-                          "<p>", df[df$go_term_label== input$inpt | substr(df$go_term_label, 1, nchar(input$inpt)) == input$inpt, "gene_synonyms"],"</p>",
-                          "<b>", " Ontology Term: ", "</b>", 
-                          "<p>", df[df$go_term_label== input$inpt | substr(df$go_term_label, 1, nchar(input$inpt)) == input$inpt, "go_term_label"],"</p>", 
-                          "<br>" ))
+            
+            updateSelectizeInput(session, "genes", choices = df[df$go_term_label== input$inpt | substr(df$go_term_label, 1, nchar(input$inpt)) == input$inpt, "gene_symbol"],
+                                 options = list(placeholder = 'start writing here...', create=FALSE), 
+                                 selected = FALSE)
+            
+            h <- isolate(input$genes)
+            
+            if (h == ""){
+                unique(paste0("<b>", "Associated gene: ", "</b>", 
+                              "<p>", df[df$go_term_label== input$inpt | substr(df$go_term_label, 1, nchar(input$inpt)) == input$inpt, "gene_symbol"], "</p>", 
+                              "<b>", " Synonyms: ", "</b>", 
+                              "<p>", df[df$go_term_label== input$inpt | substr(df$go_term_label, 1, nchar(input$inpt)) == input$inpt, "gene_synonyms"],"</p>",
+                              "<b>", " Ontology Term: ", "</b>", 
+                              "<p>", df[df$go_term_label== input$inpt | substr(df$go_term_label, 1, nchar(input$inpt)) == input$inpt, "go_term_label"],"</p>", 
+                              "<br>" ))
+            }
+            
+            
+            else {
+                unique(paste0("<b>", "Associated gene: ", "</b>",
+                              "<p>", df[df$go_term_label== h, "gene_symbol"], "</p>", 
+                              "<b>", " Synonyms: ", "</b>", 
+                              "<p>", df[df$go_term_label== h , "gene_synonyms"],"</p>",
+                              "<b>", " Ontology Term: ", "</b>", 
+                              "<p>", df[df$go_term_label== h, "go_term_label"],"</p>", 
+                              "<br>" ))}
+            
+            
+            
+            
         }
         
     }) 
